566e33cfc0884f4bac1f28d3266196df7b5fafc5
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-12-01 06:55:17 +0100

    Set up the project environment,  API credentials and state variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 
 function App() {
+  //Set up Edamam API credentials
+  const APP_ID = "4f4742f4";
+  const APP_KEY = "5b4d47477a6a4dc6e678474d5d55b429";
+  //Create state variables
+  const [recipes, setRecipes] = useState([]); //To store the list of the fetched recipes
+  const [search, setSearch] = useState(""); //To track the user’s input in the search bar
+  const [query, setQuery] = useState("pizza"); // Default query; To trigger the API call when the user submits a search.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meal Recipe App</h1>
+      <Recipe />
     </div>
   );
 }
---
+++ src/Recipe.js
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./Recipe.module.css";
+
+const Recipe = () => {
+  return (
+    <div className={style.recipe}>
+      <h1>Recipe</h1>
+    </div>
+  );
+};
+export default Recipe;
